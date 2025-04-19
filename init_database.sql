c4a0576ca3ac8aae0fcfe216b20eef3515c936ae
Tetiana Olshevska <165716610+tetiana-olshevska@users.noreply.github.com>
2025-04-20 01:01:00 +0200

    Create init_database.sql
    Initialize database and schemas

---
+++ init_database.sql
@@ -0,0 +1,7 @@
+-- Create database 'DataWarehouse' and schemas within it
+
+CREATE DATABASE DataWarehouse;
+--**************************************************************************************
+CREATE SCHEMA bronze;
+CREATE SCHEMA silver;
+CREATE SCHEMA gold;
