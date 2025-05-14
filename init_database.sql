92681225ccda06077e50989e72bca39b89ddc558
Tetiana Olshevska <165716610+tetiana-olshevska@users.noreply.github.com>
2025-05-14 21:34:42 +0200

    Fix init_database.sql

--- init_database.sql
+++ init_database.sql
@@ -1,7 +1,24 @@
--- Create database 'DataWarehouse' and schemas within it
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Create database 'DataWarehouse' and schemas within it.
+=============================================================
+*/
 
+-- Drop and recreate the 'DataWarehouse' database
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+    ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWarehouse;
+END;
+
+-- Create the 'DataWarehouse' database
 CREATE DATABASE DataWarehouse;
---**************************************************************************************
+
+-- Create Schemas
 CREATE SCHEMA bronze;
+
 CREATE SCHEMA silver;
+
 CREATE SCHEMA gold;
