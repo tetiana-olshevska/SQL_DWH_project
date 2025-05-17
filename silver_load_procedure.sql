f75c0d29616cb6f8f1cfb41f6dc6b7dea4b6f8b9
Tetiana Olshevska <165716610+tetiana-olshevska@users.noreply.github.com>
2025-05-17 13:23:52 +0200

    Change normalization approach

--- silver_load_procedure.sql
+++ silver_load_procedure.sql
@@ -36,7 +36,7 @@ BEGIN
 
     		INSERT INTO silver.crm_cust_info (
     			                                cst_id, 
-                                          cst_key, 
+                                          		cst_key, 
                                     			cst_firstname, 
                                     			cst_lastname, 
                                     			cst_marital_status, 
@@ -47,14 +47,8 @@ BEGIN
         			 cst_key,
         			 TRIM(cst_firstname) AS cst_firstname,
         			 TRIM(cst_lastname) AS cst_lastname,
-        			 CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
-        			      WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-        			      ELSE 'n/a'
-        			      END AS cst_marital_status, -- Normalize marital status values to readable format
-        			 CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-        			      WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-        			      ELSE 'n/a'
-        			      END AS cst_gndr, -- Normalize gender values to readable format
+        			 UPPER(TRIM(cst_marital_status)) AS cst_marital_status,
+        			 UPPER(TRIM(cst_gndr)) AS cst_gndr,
         			 cst_create_date
         	  FROM (SELECT *,
     			       ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
@@ -93,7 +87,7 @@ BEGIN
         	    WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
         	    WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
         	    WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-        	    ELSE 'n/a'
+        	    ELSE NULL
         	    END AS prd_line, -- Map product line codes to descriptive values
                CAST(prd_start_dt AS DATE) AS prd_start_dt,
                CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 AS DATE) AS prd_end_dt -- Calculate end date as one day before the next start date
@@ -164,9 +158,9 @@ BEGIN
       		 CASE WHEN bdate > GETDATE() THEN NULL
       		      ELSE bdate
       		      END AS bdate, -- Set future birthdates to NULL
-      		 CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-      		      WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-      		      ELSE 'n/a'
+      		 CASE WHEN UPPER(TRIM(gen)) = 'FEMALE' THEN 'F'
+      		      WHEN UPPER(TRIM(gen)) = 'MALE' THEN 'M'
+      		      ELSE NULL
       		      END AS gen -- Normalize gender values and handle unknown cases
       	    FROM bronze.erp_cust_az12;
       	  
@@ -190,11 +184,11 @@ BEGIN
                                            cntry
       		                                )
       		SELECT REPLACE(cid, '-', '') AS cid, 
-          			 CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-              				WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
-              				WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
-              				ELSE TRIM(cntry)
-              		END AS cntry -- Normalize and Handle missing or blank country codes
+          	       CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
+              		    WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
+              		    WHEN TRIM(cntry) = '' THEN NULL
+              		    ELSE TRIM(cntry)
+              		    END AS cntry -- Normalize and Handle missing or blank country codes
       		  FROM bronze.erp_loc_a101;
       	    
          SET @end_time = GETDATE();
@@ -223,13 +217,13 @@ BEGIN
       		SET @end_time = GETDATE();
       
       		PRINT '>> Load Duration: ' + CAST(DATEDIFF(SECOND, @start_time, @end_time) AS NVARCHAR) + ' seconds';
-          PRINT '>> -------------';
+                PRINT '>> -------------';
       
       		SET @batch_end_time = GETDATE();
       
       		PRINT '=========================================='
       		PRINT 'Loading Silver Layer is Completed';
-          PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
+                PRINT '   - Total Load Duration: ' + CAST(DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + ' seconds';
       		PRINT '=========================================='
     		
     END TRY
